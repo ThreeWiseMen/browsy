5df6af22c6919bf6943f24e14b92799c9c195416
Nate Smith <nwjsmith@gmail.com>
2011-05-18 15:02:10 -0400

    Wrap library in top-level module

--- lib/browsy.rb
+++ lib/browsy.rb
@@ -1,6 +1,15 @@
 module Browsy
+
+  def self.client
+  end
+
+  # Thrown when a page is instantiated without an URL
   class UrlMissingError < StandardError; end
+
+  # Thrown when a page is not found on the page
+  class ElementNotFoundError < StandardError; end
 end
 
 require 'browsy/page'
+require 'browsy/element'
 
--- lib/browsy/page.rb
+++ lib/browsy/page.rb
@@ -1,16 +1,18 @@
-class Page
-  def self.url(url)
-    @url = url
-    self
-  end
+module Browsy
+  class Page
+    def self.url(url)
+      @url = url
+      self
+    end
 
-  def self.page_url
-    @url || raise(UrlMissingError, "#{self} must have an URL")
-  end
+    def self.page_url
+      @url || raise(UrlMissingError, "#{self} must have an URL")
+    end
 
-  def initialize(*)
-    raise UrlMissingError, "#{self} must have an URL" unless self.class.page_url
-    super
+    def initialize(*)
+      raise UrlMissingError, "#{self} must have an URL" unless self.class.page_url
+      super
+    end
   end
 end
 
